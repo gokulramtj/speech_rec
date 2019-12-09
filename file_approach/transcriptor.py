06b12c8f8f94001ae1051b026f100759cc2cb7be
gokulramtj <gokulram.t.j@gmail.com>
2019-12-09 18:06:29 +0530

    Bug fixes and performance improvement

--- file_approach/transcriptor.py
+++ file_approach/transcriptor.py
@@ -6,7 +6,6 @@ import pickle
 
 r = sr.Recognizer() 
 
-
 dname = str(datetime.fromtimestamp(time.time()))
 dname = dname.replace(" ", "_")
 dname = dname.replace(":", "-")
@@ -22,29 +21,43 @@ def recognize(audio):
         f = open(name ,"a+")    
         f.write(text+" ")
         f.close()
+        os.remove(pwd+"//"+file[0])
+        os.remove(pwd+"//"+file[0].replace("audio", "flag"))
 
 
     except sr.UnknownValueError: 
         print("Could not understand audio") 
-        flag = True
+        os.remove(pwd+"//"+file[0])
+        os.remove(pwd+"//"+file[0].replace("audio", "flag"))
 
     except sr.RequestError as e: 
         print("Could not connect to internet {0}".format(e)) 
-        flag = False
      
 
 
 while True:
     for pwd, folder, file in os.walk("D://Projects//my_works//python//Speech rec projetct//file_approach//transcrpit_aud"):
         if(len(file)>0):
-            print(os.path.getsize(pwd+"//"+file[0]))
-            pickle_in = open(pwd+"//"+file[0], "rb")
-            audio = pickle.load(pickle_in)
-            pickle_in.close()
-            print("Transcripting")
-            recognize(audio)
+            pickle_in = open(pwd+"//"+file[0].replace("audio", "flag"), "rb")
+            try:
+                flag = pickle.load(pickle_in)
+                pickle_in.close()
+                if(flag==1):
+                    flag = 0
+                    pickle_in = open(pwd+"//"+file[0], "rb")
+                    audio = pickle.load(pickle_in)
+                    pickle_in.close()
+                    print(audio)
+                    print("Transcripting")
+                    recognize(audio)
 
-            os.remove(pwd+"//"+file[0])
+                else:
+                    print("waiting for Recognizer to record")
 
+            except EOFError as e:
+                print("-----------------------------------------------------------------------------------------")
+                print(e)
+                print("-----------------------------------------------------------------------------------------")
+                flag = 0
 # f.close()
 # print("Write completed.\nRefer file : " + name)
